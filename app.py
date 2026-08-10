2e8593c15209112dffbdb2ba9651fdf7347e6683
mokzwa-glitch <mokzwa@gmail.com>
2026-08-10 17:30:12 +0100

    Suppression configuration MySQL pour SQLite

--- app.py
+++ app.py
@@ -2515,17 +2515,6 @@ def telecharger_backup(nom):
     )
 
 
-# Configuration de la base de données
-database_url = os.environ.get("DATABASE_URL")
-
-if database_url:
-    database_url = database_url.replace(
-        "mysql://",
-        "mysql+pymysql://",
-        1
-    )
-
-app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 
 
 # ==================================================
