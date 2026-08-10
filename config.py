da0c1127add88f29a7e1f635b3352acfeb0c5b84
mokzwa-glitch <mokzwa@gmail.com>
2026-08-10 17:24:36 +0100

    Configuration SQLite pour Render

--- config.py
+++ config.py
@@ -1,35 +1,9 @@
 import os
 
-
 class Config:
 
-    database_url = os.getenv("DATABASE_URL")
-
-    if database_url:
-
-        # Railway peut fournir mysql://
-        # ou mysql+mysqldb://.
-        # Notre application utilise PyMySQL.
-
-        if database_url.startswith("mysql://"):
-
-            database_url = database_url.replace(
-                "mysql://",
-                "mysql+pymysql://",
-                1
-            )
-
-        elif database_url.startswith("mysql+mysqldb://"):
-
-            database_url = database_url.replace(
-                "mysql+mysqldb://",
-                "mysql+pymysql://",
-                1
-            )
-
-    SQLALCHEMY_DATABASE_URI = database_url or (
-        "mysql+pymysql://root:@localhost/schoolpay"
-    )
+    # Base de données SQLite locale à l'application
+    SQLALCHEMY_DATABASE_URI = "sqlite:///schoolpay.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
